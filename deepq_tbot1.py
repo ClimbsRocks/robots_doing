6426ea579f1edbc132de1b60c95e8306fdca1c3f
ClimbsRocks <ClimbsBytes@gmail.com>
2018-11-26 15:02:30 -0800

    first pass at switching to using deepq and openai with python3. lots of changes- let's see how this goes

---
+++ deepq_tbot1.py
@@ -0,0 +1,40 @@
+import gym
+
+from baselines import deepq
+import rospy
+
+import my_custom_env
+
+
+def callback(lcl, _glb):
+    # stop training if reward exceeds 199
+    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    return is_solved
+
+
+def main():
+
+    rospy.init_node('deep_turtle_gym', anonymous=True) #This is the line you have to add
+
+    env = gym.make('MyCustomEnvSpeed-v0')
+
+    model = deepq.models.mlp([64])
+    act = deepq.learn(
+        env,
+        q_func=model,
+        lr=1e-3,
+        max_timesteps=100000,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.02,
+        print_freq=10,
+        callback=callback
+    )
+
+    model_name = "deep_turtle1.pkl"
+    print("Saving model to {}".format(model_name))
+    act.save(model_name)
+
+
+if __name__ == '__main__':
+    main()
---
+++ python3_ws_setup.sh
@@ -0,0 +1,46 @@
+# Setup ~/python3_ws
+cd
+source /opt/ros/kinetic/setup.bash
+mkdir -p ~/python3_ws/src
+cd ~/python3_ws/
+catkin_make
+
+cd ~/python3_ws/src
+git clone https://github.com/openai/baselines.git
+cd baselines
+virtualenv venv --python=python3
+rm -rf .git
+
+source venv/bin/activate
+
+pip install tensorflow
+pip install -e .
+pip install pyyaml rospkg catkin_pkg exception defusedxml mpi4py
+
+cd ~/python3_ws/src; cd baselines
+source venv/bin/activate
+
+pip install catkin_pkg pyyaml empy rospkg numpy
+cd ~/python3_ws/src
+git clone https://github.com/ros/geometry
+git clone https://github.com/ros/geometry2
+git clone https://bitbucket.org/theconstructcore/openai_ros.git
+git clone https://bitbucket.org/theconstructcore/theconstruct_msgs.git
+rm -rf geometry/.git
+rm -rf geometry2/.git
+rm -rf openai_ros/.git
+rm -rf theconstruct_msgs/.git
+
+cd ~/python3_ws/src
+# git init
+# git remote add origin https://github.com/ClimbsRocks/learning_backup.git
+# mv CMakeLists.txt CMakeLists.txt_backup
+# git pull origin master
+
+git config --global user.email "ClimbsBytes@gmail.com"
+git config --global user.name "ClimbsRocks"
+
+cd ~/python3_ws
+rm -rf build devel
+catkin_make --force-cmake
+source devel/setup.bash
