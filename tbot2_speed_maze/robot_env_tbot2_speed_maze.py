851e144bcd77da014b1ef4ccdd00636f8e3d38eb
ClimbsRocks <ClimbsBytes@gmail.com>
2018-11-29 09:27:26 -0800

    think super is finally right, now time to fix the rst

--- tbot2_speed_maze/robot_env_tbot2_speed_maze.py
+++ tbot2_speed_maze/robot_env_tbot2_speed_maze.py
@@ -54,11 +54,12 @@ class TurtleBot2RobotEnv(robot_gazebo_env.RobotGazeboEnv):
         self.robot_name_space = "turtlebot_2_speed_maze"
 
         # We launch the init function of the Parent Class robot_gazebo_env.RobotGazeboEnv
-        super().__init__(self
-                        #  , controllers_list=self.controllers_list,
-                        # robot_name_space=self.robot_name_space,
-                        reset_controls=False,
-                        start_init_physics_parameters=False)
+        super().__init__(
+                        controllers_list=self.controllers_list
+                        , robot_name_space=self.robot_name_space
+                        , reset_controls=False
+                        , start_init_physics_parameters=False
+                        )
         # super(TurtleBot2RobotEnv, self).__init__(controllers_list=self.controllers_list,
         #                                     robot_name_space=self.robot_name_space,
         #                                     reset_controls=False,
