a0c4970daf7ac71d21f01744c39b64285734f19d
ClimbsRocks <ClimbsBytes@gmail.com>
2018-11-29 09:15:14 -0800

    tries to avoid double declaring attributes

--- tbot2_speed_maze/robot_env_tbot2_speed_maze.py
+++ tbot2_speed_maze/robot_env_tbot2_speed_maze.py
@@ -55,8 +55,8 @@ class TurtleBot2RobotEnv(robot_gazebo_env.RobotGazeboEnv):
 
         # We launch the init function of the Parent Class robot_gazebo_env.RobotGazeboEnv
         super().__init__(self
-                         , controllers_list=self.controllers_list,
-                        robot_name_space=self.robot_name_space,
+                        #  , controllers_list=self.controllers_list,
+                        # robot_name_space=self.robot_name_space,
                         reset_controls=False,
                         start_init_physics_parameters=False)
         # super(TurtleBot2RobotEnv, self).__init__(controllers_list=self.controllers_list,
